8fb7ebdba6639095841737aa5a1fc63f2e7d656b
MadCoder7 <shankarpubg4@gmail.com>
2025-12-19 14:38:21 +0530

    major changes

--- src/db.js
+++ src/db.js
@@ -1,12 +1,9 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(
-  process.env.APP_DB_NAME,
-  process.env.APP_DB_USER,
-  process.env.APP_DB_PASSWORD || "shankar" || "Shankar",
+ process.env.APP_DB_URL,
   {
-    host: process.env.APP_DB_HOST,
-    port: Number(process.env.APP_DB_PORT),
+
     dialect: "postgres",
     logging: false,
   }
